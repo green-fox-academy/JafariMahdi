3f3224136202dda6a5cd947cfa5681df9b8de83a
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 14:59:55 +0100

    Floats

---
+++ week-01/day-03/Floats.ts
@@ -0,0 +1,10 @@
+// Positive, negative and zero 
+
+console.log(3.1415); // print 3.1415
+console.log(-1.5); // print -1.5
+console.log(0.0);  // print 0.0
+
+
+// Leading and closing zero is not mandatory
+console.log(.5); // print 0.5 
+console.log(5.);  // print 5.0
