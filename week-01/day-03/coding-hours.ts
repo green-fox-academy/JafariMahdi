d1a686419ba47c7755a978b4bfbfee9c86a92ca2
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 16:42:18 +0100

    coding-hours

---
+++ week-01/day-03/coding-hours.ts
@@ -0,0 +1,10 @@
+'use strict ';
+
+let semster: number= 17;
+let daily: number= 6 * 5;
+let weekly: number= 52; 
+let all: number= semster * weekly;
+
+console.log('student spend '+ daily*semster + ' hours in semester for coding');
+
+console.log('coding hours is  %'+ all / 100);
