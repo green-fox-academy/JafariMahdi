d6d9dc22edaac149c883c3eec50208e4fe63f6dc
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 15:20:33 +0100

    Boolean

---
+++ week-01/day-03/Boolean.ts
@@ -0,0 +1,20 @@
+// true, false
+console.log(true); // Prints true
+console.log(false); // Prints false
+
+// Boolean Operators
+// Negation
+console.log(!true); // Prints false
+console.log(!false); // Prints true
+
+// And 
+console.log(true && true); // prints true
+console.log(true && false); // prints false 
+console.log(false && true); // prints false 
+console.log(false && false); // prints false
+
+// or 
+console.log(true || true); // prints true
+console.log(true || false); // prints true
+console.log(false || true); // Prints true
+console.log(false || false); // Prints false
