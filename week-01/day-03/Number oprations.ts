33fcdaca3efe040ca30d6244205f637bbd7eab5c
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 15:05:15 +0100

    Number oprations

---
+++ week-01/day-03/Number oprations.ts
@@ -0,0 +1,7 @@
+// Arithmetic operations
+console.log(1 + 1); // Prints 2
+console.log(5 - 2); // Prints 3
+console.log(3 * 4); // Prints 12
+console.log(6 / 2); // prints 3
+console.log(5 / 2); // prints 2.5
+console.log(5 % 2); // prints 1
