9333e955a85744d587beeecb0712cdf547062e45
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 14:53:22 +0100

    integers

---
+++ week-01/day-03/integers.ts
@@ -0,0 +1,11 @@
+console.log(42); // print 42
+console.log(-1); //print -1
+console.log(0) //print 0
+
+console.log(12345678987654321); //print 12345678987654321
+
+//the maximal number that is not rounded (the maximum safe integer) is : 
+// 9007199254740991
+// The reasoning behind that number is that JavaScript uses double-precision
+// floating-point format numbers
+// Link: http://en.wikipedia.org/wiki/Double_precision_floating-point_format
