ef663b37da2d601131bd349cb3a6763e7652fbe5
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 14:44:15 +0100

    Types

---
+++ week-01/day-03/Types.ts
@@ -0,0 +1,8 @@
+//print string to the terminal window
+console.log("Hello world!");
+// print an integer to the terminal window
+console.log(42);
+// prints a floating point number 
+console.log(3.141592);
+// a boolean 
+console.log(true);
