f0c0a84db47f61a44cf2a721983b1ffc5e1b6fc8
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 17:02:20 +0100

    Varible

---
+++ week-01/day-03/Varible.ts
@@ -0,0 +1,30 @@
+// string 
+let welcome: string= 'Hello, world';
+console.log(welcome);
+
+//Boolean 
+let isAwsome: boolean= true;
+console.log(isAwsome);
+
+// Integer 
+let theMeaningOfLifeAndTheUniverseAndEverything: number= 42;
+console.log(theMeaningOfLifeAndTheUniverseAndEverything);
+
+//Floating point number 
+let pi:number= 3.141592;
+console.log(pi);
+
+// Assigning and creating a variable, (define its value)
+let number:number= 12;
+
+// Mutate a variable, (redefine its value)
+number = 23 ; 
+
+// Any 
+let dynamic: any = 5;
+dynamic = true;
+dynamic = 'strange';
+
+console.log(dynamic);
+
+
