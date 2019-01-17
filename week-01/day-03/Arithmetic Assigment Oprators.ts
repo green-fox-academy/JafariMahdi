f010c04b682b901ad885e75286a13e8c3b17addb
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 17:13:08 +0100

    Arithmetic Assigment Oprators

---
+++ week-01/day-03/Arithmetic Assigment Oprators.ts
@@ -0,0 +1,41 @@
+let a: number = 12;
+a = a + 4 ;
+console.log(a); // prints 16 
+
+let b: number = 12;
+b = b -4 ;
+console.log(b); // prints 8
+
+let c: number= 12;
+c *= 3 ;
+console.log(c); // prints 36
+
+let d: number = 12 ;
+d /= 3;
+console.log(d); //prints 4 
+
+let e: number= 12 ;
+e %= 7;
+console.log(e); // prints 5 
+
+let f: number = 12;
+f++;
+console.log(f); // Prints 13
+
+let g: number = 12;
+g--;
+console.log(g); // Prints 11
+
+let h: number = 10;
+console.log(h); // Prints 10
+console.log(h++); // Prints 10
+console.log(h); // Prints 11
+
+let i: number = 10;
+console.log(i); // Prints 10
+console.log(++i); // Prints 11
+console.log(i); // Prints 11
+
+
+
+
