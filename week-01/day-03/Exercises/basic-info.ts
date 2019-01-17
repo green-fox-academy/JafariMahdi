d1a67016b99b48438b40b510e58bb9522914b669
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 21:58:40 +0100

    basic-info

---
+++ week-01/day-03/Exercises/basic-info.ts
@@ -0,0 +1,17 @@
+'use strict ';
+
+// Define several things as a variable, then print their values 
+
+let Name :String = 'mahdi';
+let age: number = 31;
+let height: any = 1.83;
+let married: boolean = false;
+
+console.log('my name is: '+ Name);
+console.log('age: '+ age);
+console.log('height: '+ height);
+console.log('married: '+ married);
+
+
+
+
