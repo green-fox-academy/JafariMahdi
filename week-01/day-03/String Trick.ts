73677f6f97ae8ff3098450373da61da0a59f1e8e
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 15:39:32 +0100

    String Trick

---
+++ week-01/day-03/String Trick.ts
@@ -0,0 +1,12 @@
+console.log('apple'); // prints apple 
+console.log('don\'t '); // prints don't
+console.log('"Everything you can imagines is reall." -Picaso'); 
+// prints : "Everything you canimagines is reall." -picaso
+
+     // String Oprators 
+     // Concatenation 
+      console.log('tooth'+ 'brush'); // prints toothbrush
+    // Concat string with number 
+    console.log('My favorite number is: ' + 8); 
+    // prints My favorite number is: 8 
+    
