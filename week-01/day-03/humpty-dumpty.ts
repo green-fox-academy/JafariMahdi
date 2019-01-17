d23fce111a7906e13f393aa15499ad746109ff73
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-01-17 14:31:15 +0100

    Humpty Dumpty

---
+++ week-01/day-03/humpty-dumpty.ts
@@ -0,0 +1,4 @@
+console.log("All the king\'s horses and all the king\'s men");
+console.log("Humpty Dumpty had a great fall.");
+console.log("Humpty sat on a wall,");
+console.log("Couldn\'t put Humpty together again.");
